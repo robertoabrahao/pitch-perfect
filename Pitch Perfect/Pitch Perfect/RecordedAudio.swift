e51ece9a742f1bab1696ab23cb2cfcd37001e432
robertoabrahao <roberto.abrahao@ge.com>
2015-03-30 07:35:25 -0300

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,20 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Roberto Abrahao on 3/12/15.
+//  Copyright (c) 2015 GE. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio {
+    var filePathUrl: NSURL!
+    var title: String!
+    
+    init () {
+        filePathUrl = NSURL.fileURLWithPath("")
+        title = ""
+        
+    }
+}
